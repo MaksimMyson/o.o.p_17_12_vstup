8ded1a62064f7e2e858ae4e4e7881264d7ff8b2f
MaksimMyson <maksimmyson33@gmail.com>
2023-12-17 14:47:06 +0200

    ex2 apdate

--- main.cpp
+++ main.cpp
@@ -3,25 +3,55 @@
 int main() {
     PhoneBook phoneBook;
 
-    Contact contact1("John Doe", "123-456-7890", "456-789-0123", "789-012-3456", "Friend");
-    Contact contact2("Jane Smith", "111-222-3333", "444-555-6666", "777-888-9999", "Colleague");
+    int choice;
+    do {
+        std::cout << "\nPhonebook Menu:\n"
+            << "1. Add Contact\n"
+            << "2. Display All Contacts\n"
+            << "3. Save to File\n"
+            << "4. Load from File\n"
+            << "5. Exit\n"
+            << "Enter your choice: ";
+        std::cin >> choice;
 
-    phoneBook.addContact(contact1);
-    phoneBook.addContact(contact2);
+        switch (choice) {
+        case 1: {
+            std::string name, home, work, mobile, info;
+            std::cin.ignore(); // Clear newline from previous input
+            std::cout << "Enter Full Name: ";
+            std::getline(std::cin, name);
+            std::cout << "Enter Home Phone: ";
+            std::getline(std::cin, home);
+            std::cout << "Enter Work Phone: ";
+            std::getline(std::cin, work);
+            std::cout << "Enter Mobile Phone: ";
+            std::getline(std::cin, mobile);
+            std::cout << "Enter Additional Info: ";
+            std::getline(std::cin, info);
 
-    // Display all contacts
-    std::cout << "All Contacts:\n";
-    phoneBook.displayAllContacts();
-
-    // Save data to file
-    phoneBook.saveToFile("contacts.txt");
-
-    // Load data from file
-    phoneBook.loadFromFile("contacts.txt");
-
-    // Display all contacts after loading from file
-    std::cout << "\nAll Contacts After Loading:\n";
-    phoneBook.displayAllContacts();
+            Contact newContact(name, home, work, mobile, info);
+            phoneBook.addContact(newContact);
+            break;
+        }
+        case 2:
+            std::cout << "All Contacts:\n";
+            phoneBook.displayAllContacts();
+            break;
+        case 3:
+            phoneBook.saveToFile("contacts.txt");
+            break;
+        case 4:
+            phoneBook.loadFromFile("contacts.txt");
+            std::cout << "\nAll Contacts After Loading:\n";
+            phoneBook.displayAllContacts();
+            break;
+        case 5:
+            std::cout << "Exiting the program.\n";
+            break;
+        default:
+            std::cout << "Invalid choice. Please enter a number between 1 and 5.\n";
+        }
+    } while (choice != 5);
 
     return 0;
 }
